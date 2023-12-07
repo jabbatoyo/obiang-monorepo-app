c31029b44ef08d3b9289e0ea935235628bfa8918
Juan Obiang <juanobiang@MacBook-Pro-de-Juan.local>
2023-12-07 03:16:56 +0100

    feat(obiang-library): import all the component for external use

---
+++ obiang-library/lib/index.ts
@@ -0,0 +1,4 @@
+export { default as Button } from "./components/Button";
+export { default as Loading } from "./components/Loading";
+export { default as Pagination } from "./components/Pagination";
+export { default as Table } from "./components/Table";
